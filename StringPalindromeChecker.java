61f2ad20325e61f50fd9d0f8436955de53d8f92a
daniel georgiev <danzgeorg.contact@gmail.com>
2024-10-22 11:31:46 +0100

    Upploaded a new file
    This is my first proper project in Java.

---
+++ StringPalindromeChecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringPalindromeChecker {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String string = input.nextLine();
+
+        if (isPalindrome(string)){
+            System.out.println("The string is palindrome");
+        }
+        else {
+            System.out.println("The string is not palindrome");
+        }
+    }
+    public static boolean isPalindrome(String str){
+        int length = str.length();
+        for (int i = 0; i < length /2; i++){
+            if (str.charAt(i) != str.charAt(length - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
